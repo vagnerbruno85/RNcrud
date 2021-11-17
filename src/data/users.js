d853b5a6d9bbad853da23990067fe9866a85b686
vagnerbruno85 <mr.vagnerbruno@gmail.com>
2021-11-17 15:34:41 -0300

    Mock: users

---
+++ src/data/users.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        name: 'João da Silva',
+        email: 'josil@expmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+    },
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email: 'maragu@expemail.com',
+        avatarUrl: "https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_960_720.png",
+    },
+    {
+        id: 3,
+        name: 'Julio Fragoso',
+        email: 'jufra@expemail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+    },
+    {
+        id: 4,
+        name: "Rafael Monteiro",
+        email: "ramon@expemail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png" ,
+    },
+    {
+        id: 5,
+        name: 'Gabriel Bruno',
+        email: "gabru@expemail.com",
+        avatarUrl:"https://cdn.pixabay.com/photo/2018/09/14/23/28/avatar-3678347_960_720.png" ,
+    },
+    {
+        id: 6,
+        name: 'Laura da Silva',
+        email: "lausi@expemal.com",
+        avatarUrl: 'https://cdn.pixabay.com/photo/2020/10/12/19/27/girl-5649922_960_720.png',
+    },
+]
