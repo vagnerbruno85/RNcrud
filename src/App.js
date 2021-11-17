4cca6f2a383e0d2c3c8955f7cc2bb3d7f7f4b87d
vagnerbruno85 <mr.vagnerbruno@gmail.com>
2021-11-17 14:06:32 -0300

    Structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator  } from '@react-navigation/native-stack'
+
+
+
+export default props => {
+    return (
+        <View>
+
+        </View>
+    );
+}
